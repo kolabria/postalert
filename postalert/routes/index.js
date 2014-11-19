cf54a501c33c57823ade0d767ff7959d2d7159b0
Ted Judge <tedjudge@kolabria.com>
2014-11-19 14:33:16 -0500

    Change to use template engine with included plain html
    This should fix issue of routing with /

--- postalert/routes/index.js
+++ postalert/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  //res.render('index', { title: 'Express' });
-  res.sendfile('public/index.html');
+  res.render('index');
+ // res.sendfile('public/index.html');
 });
 
 module.exports = router;
