442a819e1e53149c4335a8f5c731f90ba2186f8c
Ted Judge <tedjudge@kolabria.com>
2014-11-20 14:56:51 -0500

    Add signup and postal code modal
    improvements to images

--- postalert/routes/index.js
+++ postalert/routes/index.js
@@ -7,4 +7,26 @@ router.get('/', function(req, res) {
  // res.sendfile('public/index.html');
 });
 
+
+/* post home page. */
+router.post('/', function(req, res) {
+    console.log('postal code: ',req.body.InputPostalCode);
+  res.render('signup');
+ // res.sendfile('public/index.html');
+});
+
+/* post home page. */
+router.get('/:pc?', function(req, res) {
+    if (req.params.pc){
+        console.log('postal code: ', req.params.pc) ;
+        res.render('signup');
+    }    
+    else
+    {
+        // send back something that says to enter a vailid postal code. Need to check too.  
+        res.render('signup');  
+    }
+
+});
+
 module.exports = router;
