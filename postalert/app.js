8382dd05acc9eae3581dae3ea656966512435cab
Ted Judge <tedjudge@kolabria.com>
2015-01-08 14:07:35 -0500

    new jade templates & translations
    switch from html to jade templates
    auto lang detection & button
    french and english

--- postalert/app.js
+++ postalert/app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var locale = require("locale");
+var supported = ["en","fr"];
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users');
 
 var app = express();
 
@@ -21,9 +24,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(locale(supported));
+app.use(session({
+  secret: 'wiliwonka',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- postalert/routes/index.js
+++ postalert/routes/index.js
@@ -1,24 +1,59 @@
 var express = require('express');
 var router = express.Router();
+var IpInfo = require("ipinfo");
+var session = require('express-session');
+
+var locale = require("locale");
+var supported = ["en","fr"];
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
- // res.sendfile('public/index.html');
+  console.log('index '); 
+  console.log('referer: ',req.headers.referer);
+  if (!req.session.lang) {
+      req.session.lang = req.locale;
+      console.log("The best match is: " + req.locale);
+  }
+   
+  res.render('index',{
+      lang: req.session.lang
+  });
+
 });
 
 
-/* post home page. */
-router.post('/', function(req, res) {
-    console.log('postal code: ',req.body.InputPostalCode);
-  res.render('signup');
- // res.sendfile('public/index.html');
-});
+
+router.get("/lang", function(req, res) {
+    
+    req.session.lang = req.locale;
+    
+    console.log("You asked for: " + req.headers["accept-language"]);
+    console.log("We support: " + supported);
+    console.log( "Our default is: " + locale.Locale["default"]);
+    console.log("The best match is: " + req.locale);
+    if (req.locale == 'en')
+        console.log("got english")
+    else
+        console.log("got something else")
+    
+  res.header("Content-Type", "text/plain")
+  res.send(
+    "You asked for: " + req.headers["accept-language"] + "\n" +
+    "We support: " + supported + "\n" +
+    "Our default is: " + locale.Locale["default"] + "\n" +
+    "The best match is: " + req.locale + "\n" +
+    "The saved lang is: " + req.session.lang
+  )
+})
+
 
 /* post home page. */
+/*
 router.get('/:pc?', function(req, res) {
     if (req.params.pc){
-        console.log('postal code: ', req.params.pc) ;
+        console.log('postal code:bla bla ', req.params.pc) ;
+        
         res.render('signup');
     }    
     else
@@ -28,23 +63,47 @@ router.get('/:pc?', function(req, res) {
     }
 
 });
-
+*/
 /* GET signup page. */
-router.get('/signup', function(req, res) {
-  res.render('signup');
+router.get('/signup/:lang?', function(req, res) {
+
+   if (req.params.lang)
+     req.session.lang = req.params.lang;
+  res.render('signup',{
+      lang: req.session.lang
+  });
 });
 
-/* post home page. */
+/* GET thankyou page.  -   need this for forms */
+router.get('/thankyou/:lang?', function(req, res) {
+
+  if (req.params.lang)
+    req.session.lang = req.params.lang;
+  res.render('thankyou',{
+      lang: req.session.lang
+  });
+});
+
+/* post thankyou page. */
 router.post('/po', function(req, res) {
-    //console.log('postal code: ',req.body.InputPostalCode);
-  res.render('thankyou');
+  
+  res.render('thankyou',{
+      lang: req.session.lang
+  });
 
 });
 
 
-/* GET thankyou page. */
-router.get('/thankyou', function(req, res) {
-  res.render('thankyou');
+router.get('/:lang?', function(req, res) {    
+    console.log('url: ',req.url);
+    console.log('referer: ',req.headers.referer);
+  console.log('lang pref', req.params.lang); 
+  req.session.lang = req.params.lang;
+    
+  res.render('index',{
+      lang: req.session.lang
+  });
+
 });
 
 module.exports = router;
